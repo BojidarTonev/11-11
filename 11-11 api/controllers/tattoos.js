e3cc345b7cdc98337b1e869354a5518a8d56b760
Bojidar Tonev <ghost1993ifyy@gmail.com>
2020-02-09 13:28:59 +0200

    Made changes to controller and router to accept and return tattoo entities from specific style

--- 11-11 api/controllers/tattoos.js
+++ 11-11 api/controllers/tattoos.js
@@ -1,3 +1,4 @@
+var url = require('url');
 const Tattoos = require("../models/Tattoos");
 
 module.exports = {
@@ -10,6 +11,16 @@ module.exports = {
       } catch (error) {
         res.status(500).json({ message: err.message });
       }
+    },
+    tatoosFromStyle: (req, res, next) => {
+        var style = url.parse(req.url, true).pathname.split('/')[2];
+        try {
+          Tattoos.find({style: style})
+          .then(tattoos => res.send(tattoos))
+          .catch(next);
+        } catch (error) {
+          res.status(500).json({ message: err.message });
+        }
     }
   }
 };
--- 11-11 api/routes/tattoos.js
+++ 11-11 api/routes/tattoos.js
@@ -2,5 +2,6 @@ const controllers = require('../controllers');
 const router = require('express').Router();
 
 router.get('/tattoos/all', controllers.tattoos.get.allTattoos)
+router.get('/tattoos/:style', controllers.tattoos.get.tatoosFromStyle)
 
 module.exports = router;
